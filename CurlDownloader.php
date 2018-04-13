9f772b6d96024b3905cda255ce4a84c880ac0388
Chihiro Koezuka <koexuka@gmail.com>
2018-04-13 19:13:51 +0900

    CurlDownloader.php追加

---
+++ CurlDownloader.php
@@ -0,0 +1,35 @@
+<?php
+class Downloader
+{
+	public static function downloadAndSave($url, $savepath)
+	{   
+		$fp = fopen($savepath, "w");
+		if (!$fp)
+		{   
+			return false;
+		}   
+		$option = [ 
+			CURLOPT_TIMEOUT         => 10, 
+			CURLOPT_RETURNTRANSFER  => true,
+			CURLOPT_FILE            => $fp,
+		];  
+		$ch = curl_init($url);
+		curl_setopt_array($ch, $option);
+		$data = curl_exec($ch);
+		$errNo= curl_error($ch);
+		curl_close($ch);
+		fclose($fp);
+
+		// エラー発生
+		if (!$data)
+		{   
+			return false;
+		}   
+		if ($errNo!=CURLE_OK)
+		{   
+			return false;
+		}   
+
+		return true;
+	}   
+}
